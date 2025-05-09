7ac086e0ba11fad723747fe238c24f0130796a6c
rinz <roberts.denisovs@gmail.com>
2025-05-09 11:40:54 +0300

    bunch of new stuff, cuda pipeline working

--- src/buffers.zig
+++ src/buffers.zig
@@ -4,7 +4,7 @@ pub const RHLOBuffer = struct {
     device: bool,
 };
 
-pub fn create_buffer(data_ptr: *void, size: usize) RHLOBuffer {
+pub fn createBuffer(data_ptr: *void, size: usize) RHLOBuffer {
     return .{
         .data_ptr = data_ptr,
         .size = size,
--- src/root.zig
+++ src/root.zig
@@ -15,95 +15,166 @@ const Builder = @import("builder.zig").Builder;
 var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
 
 pub fn execute(program: nodes.RHLOProgram, input_buffer: buffers.RHLOBuffer, output_buffer: buffers.RHLOBuffer) void {
-    // copy data over to device if buffers are passed as host data
-    // if (input_buffer.device == false) {}
-    // if (output_buffer.device == false) {}
-
     _ = program;
-    _ = input_buffer;
-    _ = output_buffer;
+
+    _ = target.LLVMInitializeNativeTarget();
+    _ = target.LLVMInitializeNativeAsmPrinter();
+    _ = target.LLVMInitializeNativeAsmParser();
+
+    _ = rllvm.llvm.support.LLVMLoadLibraryPermanently("/run/opengl-driver/lib/libcuda.so");
+
+    const module = core.LLVMModuleCreateWithName("main");
+
+    var param_types: [2]types.LLVMTypeRef = .{
+        core.LLVMPointerType(core.LLVMVoidType(), 0),
+        core.LLVMPointerType(core.LLVMVoidType(), 0),
+    };
+    const fn_type = core.LLVMFunctionType(core.LLVMInt32Type(), &param_types, 2, 0);
+    const function = core.LLVMAddFunction(module, "main", fn_type);
+
+    const entry = core.LLVMAppendBasicBlock(function, "entry");
+
+    const builder = core.LLVMCreateBuilder();
+    defer core.LLVMDisposeBuilder(builder);
+    core.LLVMPositionBuilderAtEnd(builder, entry);
+
+    try cuda.init(module, builder);
+    const cuda_device = try cuda.deviceGet(module, builder);
+    const cuda_context = try cuda.contextCreate(module, builder, cuda_device);
+    _ = cuda_context;
+
+    const h_input = rllvm.types.OpaqueRef{ .ref = core.LLVMGetParam(function, 0) };
+    const d_input = rllvm.types.CudaValueRef.create(builder);
+    const d_output = rllvm.types.CudaValueRef.create(builder);
+
+    const four: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(core.LLVMInt64Type(), 4, 0) };
+    // const size_bytes = rllvm.types.IntegerRef{ .ref = core.LLVMConstInt(core.LLVMInt32Type(), 4, 0) };
+    if (input_buffer.device == false) {
+        try cuda.memAlloc(module, builder, d_input, four);
+        try cuda.copyHToD(module, builder, d_input, h_input, four);
+    }
+    if (output_buffer.device == false) {
+        try cuda.memAlloc(module, builder, d_output, four);
+    }
+
+    const ptx =
+        \\//
+        \\.version 8.4
+        \\.target sm_52
+        \\.address_size 64
+        \\
+        \\.visible .entry main(
+        \\  .param .u64 input_ptr,
+        \\  .param .u64 output_ptr
+        \\)
+        \\{
+        \\  .reg .b32 %r<2>;
+        \\  .reg .b64 %rd<3>;
+        \\
+        \\  ld.param.u64 %rd1, [input_ptr];
+        \\  ld.param.u64 %rd2, [output_ptr];
+        \\
+        \\  cvta.to.global.u64 %rd1, %rd1;
+        \\  cvta.to.global.u64 %rd2, %rd2;
+        \\
+        \\  ld.global.u32 %r1, [%rd1];
+        \\
+        \\  st.global.u32 [%rd2], %r1;
+        \\
+        \\  ret;
+        \\}
+    ;
+
+    const kernel_len = ptx.len;
+    const global_ptx_str = core.LLVMAddGlobal(module, core.LLVMPointerType(core.LLVMInt8Type(), 0), "ptx_str");
+    const kernel_constant = core.LLVMConstString(@ptrCast(ptx), @intCast(kernel_len), 0);
+    core.LLVMSetInitializer(global_ptx_str, kernel_constant);
+
+    const cuda_module = try cuda.moduleLoadData(module, builder, .{ .ref = global_ptx_str });
+
+    const cuda_function = try cuda.moduleGetFunction(module, builder, cuda_module);
+
+    const int_type = core.LLVMInt32Type();
+    const grid_dim_x: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const grid_dim_y: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const grid_dim_z: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const block_dim_x: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const block_dim_y: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const block_dim_z: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 1, 0) };
+    const shared_mem_bytes: rllvm.types.IntegerRef = .{ .ref = core.LLVMConstInt(int_type, 0, 0) };
+    var kernel_params = [_]rllvm.types.CudaValueRef{ d_input, d_output };
+    try cuda.launchKernel(module, builder, cuda_function, grid_dim_x, grid_dim_y, grid_dim_z, block_dim_x, block_dim_y, block_dim_z, shared_mem_bytes, &kernel_params);
+
+    const result_ptr = rllvm.types.OpaqueRef{
+        .ref = core.LLVMGetParam(function, 1),
+    };
+    try cuda.copyDToH(module, builder, d_output, result_ptr, four);
+
+    // const float_type = core.LLVMFloatType();
+    const zero_idx = core.LLVMConstInt(core.LLVMInt64Type(), 0, 0);
+    // var indices = [_]types.LLVMValueRef{zero_idx};
+    // const first_element_ptr = core.LLVMBuildGEP2(builder, float_type, result_ptr.ref, &indices[0], indices.len, "first_element_ptr");
+    // const first_element = core.LLVMBuildLoad2(builder, float_type, first_element_ptr, "first_element");
+
+    // _ = core.LLVMBuildStore(builder, zero_idx, result_ptr.ref);
+
+    _ = core.LLVMBuildRet(builder, zero_idx);
+
+    // execution
+    var error_msg: [*c]u8 = null;
+    var engine: types.LLVMExecutionEngineRef = undefined;
+    if (execution.LLVMCreateExecutionEngineForModule(&engine, module, &error_msg) != 0) {
+        std.debug.print("Execution engine creation failed: {s}\n", .{error_msg});
+        core.LLVMDisposeMessage(error_msg);
+        @panic("failed to create exec engine");
+    }
+    defer execution.LLVMDisposeExecutionEngine(engine);
+
+    _ = core.LLVMDumpModule(module);
+
+    const main_addr = execution.LLVMGetFunctionAddress(engine, "main");
+    const MainFn = fn (*void, *void) callconv(.C) f32;
+    const main_fn: *const MainFn = @ptrFromInt(main_addr);
+
+    _ = main_fn(input_buffer.data_ptr, output_buffer.data_ptr);
 }
 
 pub fn createBuilder() Builder {
     return Builder.init(arena.allocator());
 }
 
-// this seems weird, but it'll make sense when we consider how the c api will work. or it won't. in that case TODO: make this make sense
-pub fn createShape(dims: []const usize) nodes.Shape {
-    return dims;
-}
-
-test "anoda one" {
+test "matmul" {
     var builder = createBuilder();
     const dtype = nodes.DataType.F32;
-    const shape: nodes.Shape = &[_]usize{5};
+    const shape: nodes.Shape = &[_]usize{ 2, 2 };
     const param0 = try builder.paramemeter(dtype, shape);
     const param1 = try builder.paramemeter(dtype, shape);
 
     _ = try builder.opAdd(param0, param1);
-}
 
-test "cuda" {
-    _ = target.LLVMInitializeNativeTarget();
-    _ = target.LLVMInitializeNativeAsmPrinter();
-    _ = target.LLVMInitializeNativeAsmParser();
+    const input_buffer = try arena.allocator().alloc(f32, 4);
+    input_buffer[0] = 1.0;
+    input_buffer[1] = 2.0;
+    input_buffer[2] = 3.0;
+    input_buffer[3] = 4.0;
+    const output_buffer = try arena.allocator().alloc(f32, 4);
+    output_buffer[0] = 9;
 
-    const module = core.LLVMModuleCreateWithName("main");
+    std.debug.print("in matmul: {}\n", .{output_buffer[0]});
+    std.debug.print("d_output: {any}\n", .{output_buffer.ptr});
+    execute(
+        builder.program,
+        .{
+            .data_ptr = @ptrCast(input_buffer.ptr),
+            .device = false,
+            .size = 4,
+        },
+        .{
+            .data_ptr = @ptrCast(output_buffer.ptr),
+            .device = false,
+            .size = 4,
+        },
+    );
 
-    var param_types: [2]types.LLVMTypeRef = .{
-        core.LLVMInt32Type(),
-        core.LLVMInt32Type(),
-    };
-    const fn_type = core.LLVMFunctionType(core.LLVMInt32Type(), &param_types, 2, 0);
-    const function = core.LLVMAddFunction(module, "add", fn_type);
-
-    const entry = core.LLVMAppendBasicBlock(function, "entry");
-
-    const builder = core.LLVMCreateBuilder();
-    defer core.LLVMDisposeBuilder(builder);
-    core.LLVMPositionBuilderAtEnd(builder, entry);
-
-    cuda.callCuInit(module, builder);
-}
-
-test "fuck" {
-    _ = target.LLVMInitializeNativeTarget();
-    _ = target.LLVMInitializeNativeAsmPrinter();
-    _ = target.LLVMInitializeNativeAsmParser();
-
-    const module = core.LLVMModuleCreateWithName("main");
-
-    var param_types: [2]types.LLVMTypeRef = .{
-        core.LLVMInt32Type(),
-        core.LLVMInt32Type(),
-    };
-    const fn_type = core.LLVMFunctionType(core.LLVMInt32Type(), &param_types, 2, 0);
-    const function = core.LLVMAddFunction(module, "add", fn_type);
-
-    const entry = core.LLVMAppendBasicBlock(function, "entry");
-
-    const builder = core.LLVMCreateBuilder();
-    defer core.LLVMDisposeBuilder(builder);
-    core.LLVMPositionBuilderAtEnd(builder, entry);
-
-    const param0 = core.LLVMGetParam(function, 0);
-    const param1 = core.LLVMGetParam(function, 1);
-    const sum = core.LLVMBuildAdd(builder, param0, param1, "sum");
-    _ = core.LLVMBuildRet(builder, sum);
-
-    var error_msg: [*c]u8 = null;
-    var engine: types.LLVMExecutionEngineRef = undefined;
-    if (execution.LLVMCreateExecutionEngineForModule(&engine, module, &error_msg) != 0) {
-        std.debug.print("Execution engine creation failed: {s}\n", .{error_msg});
-        core.LLVMDisposeMessage(error_msg);
-        return error.ExecutionEngineCreationFailed;
-    }
-    defer execution.LLVMDisposeExecutionEngine(engine);
-
-    const add_addr = execution.LLVMGetFunctionAddress(engine, "add");
-    const AddFn = fn (i32, i32) callconv(.C) i32;
-    const add_fn: *const AddFn = @ptrFromInt(add_addr);
-
-    const result = add_fn(3, 4);
-    std.debug.print("Result of 3 + 4 = {}\n", .{result});
+    std.debug.print("in matmul: {}\n", .{output_buffer[0]});
 }
